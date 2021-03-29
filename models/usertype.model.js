107805abc9705b6b804b5100abd2d5c36f23d88a
Florian Sonntag <sonntag.florian@web.de>
2021-03-29 13:07:31 +0200

    + Realm Connection + Endpoint for Login / Register / Logout (+ routes) + Model for Bew/WG Save x ID

--- models/usertype.model.js
+++ models/usertype.model.js
@@ -1,20 +1,17 @@
 const mongoose = require("mongoose");
 
 const UserSchema = mongoose.Schema({
-        email: {
-            type: String,
-            required: true
-        },
-        password: {
+        uid: {
             type: String,
             required: true
         },
         type: {
             type: String,
+            required: true
         }
     },
     {
         timestamps: true
     });
 
-module.exports = mongoose.model("Users", UserSchema);
+module.exports = mongoose.model("UsersType", UserSchema);
